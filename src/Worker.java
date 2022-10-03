3dfea7ae28b08559b66fca1b4d89b92ab26940ac
umji <sarel087@protonmail.com>
2022-10-03 15:12:13 -0700

    Added toString method.

--- src/Worker.java
+++ src/Worker.java
@@ -5,34 +5,29 @@ public abstract class Worker implements Comparable {
     int id;
 
     //parameterized constructor to be inherited
-    void Worker(String name, int rating, int id){
-        if (this.rating <= 5 || this.rating > 1)
-        {
+    void Worker(String name, int rating, int id) {
+        if (this.rating <= 5 || this.rating > 1) {
             this.rating = rating;
-        }
-        else throw new IllegalArgumentException("The rating must be greater than/equal to 1, and greater than/equal to 5.");
+        } else
+            throw new IllegalArgumentException("The rating must be greater than/equal to 1, and greater than/equal to 5.");
     }
 
     /*compareTo returns positive if the worker's rating is greater than that of the worker being compared to, negative if less than, and returns the same results by
     checking the IDs of each worker if the ratings are equal. If the IDs are equal, the method will return 0.*/
-    int compareTo(Worker worker)
-    {
-    if ((worker.rating - rating) != 0) return (worker.rating - rating);
-    else
-    {
-        return (worker.id - id);
-    }
+    int compareTo(Worker worker) {
+        if ((worker.rating - rating) != 0) return (worker.rating - rating);
+        else {
+            return (worker.id - id);
+        }
     }
 
     //calculateBonus is abstract and will have no body.
     abstract void calculateBonus();
 
-
-
-
-
-
-
+    //toString, not hard-coded. This means this method can simply be inherited and function correctly.
+    public String toString() {
+        return getClass() + ": " + getClass().getName() + ", Rating: " + rating + ", ID: " + id;
+    }
 
 
 }
